7d09a1bedcbb0cd0f6f7649c6b10af4bb5c18cec
dennxa <denia.98@hotmail.com>
2021-11-29 10:50:28 -0700

    first commit

--- inc/settings.php
+++ inc/settings.php
@@ -1,19 +1,12 @@
 <?php 
-$servername = "localhost";
-$username = "id17436345_yesica";
-$password = "q(]Yy4jcy^4()5+U";
-$dbname = "id17436345_databse";
 
-function validar(){
-    session_start();
-if (empty($_SESSION["nombre"]))
-{
-  echo "Se detecto un acceso ilegal al sistema, su ip esta siendo monitoreada y sera enviada a la policia";
-  ?>
-  <a href="http://localhost/crud/">Sitio de login</a>
-  <?php
-  exit();
-}
-}
+
+
+
+
+
+
+
+
 
 ?>
